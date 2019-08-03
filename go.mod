78fa3a439426ed38fe7c95bc669935a842d509bf
Denis Nelubin <dnelubin@gmail.com>
2019-08-03 12:49:39 +0600

    Added the app

---
+++ go.mod
@@ -0,0 +1,5 @@
+module example
+
+go 1.12
+
+require github.com/gelin/gogreet v0.0.1
---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"github.com/gelin/gogreet"
+	"fmt"
+)
+
+func main() {
+	fmt.Println(greet.GreetingFor("World"))
+}
